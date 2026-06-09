c80ea84cd60fbe4d2e1ef26ca6221dc0277c110e
strippedaxles <strippedaxles@gmail.com>
2026-06-09 13:57:10 -0400

    motor spin toggle

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include "pros/abstract_motor.hpp"
+#include "pros/misc.h"
 #include "pros/motors.hpp"
 
 /**
@@ -82,9 +83,18 @@ void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-
+	static bool motorToggle = false;
 
 	while (true) {
+		if (master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_A)) {
+			motorToggle = !motorToggle;
+			if(motorToggle == true){
+				motor.move(127);
+			}else{
+				motor.brake();
+			}
+		}
+
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
